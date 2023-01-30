d4d35ded5ff0697db685a772ca3202b6ae248f51
Melany Estevez Camata <63152500+Mademoisellefou@users.noreply.github.com>
2023-01-30 12:04:48 -0400

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
 
     const navigate = useNavigate();
     const location = useLocation();
-    const from = location.state?.from?.pathname || "/";
+    const from = location.pathname || "/";
 
     const userRef = useRef();
     const errRef = useRef();
